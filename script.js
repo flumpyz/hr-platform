eb64b70c1f3e0244179e5c5cf28e8ae20c78b4eb
flumpyz <natalianata2001@mail.ru>
2022-06-16 11:01:40 +0500

    fixed startup script.js

--- script.js
+++ script.js
@@ -2,28 +2,23 @@ const baseApiUrl = 'http://localhost:8000/api'
 let token
 
 
-let result = await fetch(baseApiUrl + '/auth/registration?name=test7&email=test7@example.com&password=test', {
+let result = await fetch(baseApiUrl + '/auth/registration?name=test&email=test@example.com&password=test', {
     method: 'POST',
     headers: new Headers({
         Accept: 'application/json'
     }),
 })
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
-result = await fetch(baseApiUrl + '/auth/login', {
+result = await fetch(baseApiUrl + '/auth/login?email=test@example.com&password=test', {
     method: 'POST',
     headers: new Headers({
         Accept: 'application/json'
     }),
-    body: JSON.stringify({
-        email: 'test7@example.com',
-        password: 'test'
-    })
 });
-console.log(result)
-result = result.json()
+result = await result.json()
 token = result.access_token
 console.log(result)
 
@@ -39,21 +34,17 @@ result = await fetch(baseApiUrl + '/auth/logout', {
         Authorization: "Bearer " + token
     })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
-result = await fetch(baseApiUrl + '/auth/login', {
+result = await fetch(baseApiUrl + '/auth/login?email=test@example.com&password=test', {
     method: 'POST',
     headers: new Headers({
         Accept: 'application/json'
     }),
-    body: JSON.stringify({
-        email: 'test@example.com',
-        password: 'test'
-    })
 });
-result = result.json()
+result = await result.json()
 token = result.access_token
 console.log(result)
 
@@ -61,24 +52,22 @@ console.log(result)
 result = await fetch(baseApiUrl + '/auth/me', {
     method: 'POST',
     headers: new Headers({
-        Accept: 'application/json'
+        Accept: 'application/json',
+        Authorization: "Bearer " + token
     }),
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
-result = await fetch(baseApiUrl + '/studyDirections', {
+result = await fetch(baseApiUrl + '/studyDirections?title=frontend', {
     method: 'POST',
     headers: new Headers({
         Accept: 'application/json',
         Authorization: "Bearer " + token
     }),
-    body: JSON.stringify({
-        title: 'frontend',
-    })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
@@ -89,7 +78,7 @@ result = await fetch(baseApiUrl + '/studyDirections', {
         Authorization: "Bearer " + token
     })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
@@ -100,21 +89,18 @@ result = await fetch(baseApiUrl + '/studyDirections/1', {
         Authorization: "Bearer " + token
     })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
-result = await fetch(baseApiUrl + '/mentors', {
+result = await fetch(baseApiUrl + '/mentors?user_id=1', {
     method: 'POST',
     headers: new Headers({
         Accept: 'application/json',
         Authorization: "Bearer " + token
     }),
-    body: JSON.stringify({
-        user_id: 1,
-    })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
@@ -125,7 +111,7 @@ result = await fetch(baseApiUrl + '/mentors', {
         Authorization: "Bearer " + token
     })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
@@ -136,20 +122,16 @@ result = await fetch(baseApiUrl + '/mentors/1', {
         Authorization: "Bearer " + token
     })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
 
 
-result = await fetch(baseApiUrl + '/mentors/add-study-direction', {
+result = await fetch(baseApiUrl + '/mentors/add-study-direction?study_direction_id=1&years_experience=3', {
     method: 'POST',
     headers: new Headers({
         Accept: 'application/json',
         Authorization: "Bearer " + token
     }),
-    body: JSON.stringify({
-        study_direction_id: 1,
-        years_experience: 3,
-    })
 });
-result = result.json()
+result = await result.json()
 console.log(result)
